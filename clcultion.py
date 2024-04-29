3ae8224869bfe4538d1ab946924dc575debcf1e6
Aayush-2103 <2103aayush@gmail.com>
2024-04-29 13:13:10 +0530

    Understanding the digits algorithm updated

--- clcultion.py
+++ clcultion.py
@@ -1,60 +1,63 @@
 def calculation(numbers, correct_order, operator):
+    if numbers:
+        for i in range(len(correct_order)):
+            if correct_order[i] == '**' or correct_order[i] == '^':
+                index = operator.index(correct_order[i])
+                ans = numbers[index] ** numbers[index+1]
+                numbers.pop(index)
+                numbers.pop(index)
+                numbers.insert(index, ans)
+                operator.pop(index)
 
-    for i in range(len(correct_order)):
-        if correct_order[i] == '**' or correct_order[i] == '^':
-            index = operator.index(correct_order[i])
-            ans = numbers[index] ** numbers[index+1]
-            numbers.pop(index)
-            numbers.pop(index)
-            numbers.insert(index, ans)
-            operator.pop(index)
+            elif correct_order[i] == '/':
+                index = operator.index(correct_order[i])
+                ans = numbers[index] / numbers[index+1]
+                numbers.pop(index)
+                numbers.pop(index)
+                numbers.insert(index, ans)
+                operator.pop(index)
 
-        elif correct_order[i] == '/':
-            index = operator.index(correct_order[i])
-            ans = numbers[index] / numbers[index+1]
-            numbers.pop(index)
-            numbers.pop(index)
-            numbers.insert(index, ans)
-            operator.pop(index)
+            elif correct_order[i] == '//':
+                index = operator.index(correct_order[i])
+                ans = numbers[index] // numbers[index+1]
+                numbers.pop(index)
+                numbers.pop(index)
+                numbers.insert(index, ans)
+                operator.pop(index)
 
-        elif correct_order[i] == '//':
-            index = operator.index(correct_order[i])
-            ans = numbers[index] // numbers[index+1]
-            numbers.pop(index)
-            numbers.pop(index)
-            numbers.insert(index, ans)
-            operator.pop(index)
+            elif correct_order[i] == '%':
+                index = operator.index(correct_order[i])
+                ans = numbers[index] % numbers[index+1]
+                numbers.pop(index)
+                numbers.pop(index)
+                numbers.insert(index, ans)
+                operator.pop(index)
 
-        elif correct_order[i] == '%':
-            index = operator.index(correct_order[i])
-            ans = numbers[index] % numbers[index+1]
-            numbers.pop(index)
-            numbers.pop(index)
-            numbers.insert(index, ans)
-            operator.pop(index)
+            elif correct_order[i] == '*':
+                index = operator.index(correct_order[i])
+                ans = numbers[index] * numbers[index+1]
+                numbers.pop(index)
+                numbers.pop(index)
+                numbers.insert(index, ans)
+                operator.pop(index)
 
-        elif correct_order[i] == '*':
-            index = operator.index(correct_order[i])
-            ans = numbers[index] * numbers[index+1]
-            numbers.pop(index)
-            numbers.pop(index)
-            numbers.insert(index, ans)
-            operator.pop(index)
+            elif correct_order[i] == '+':
+                index = operator.index(correct_order[i])
+                ans = numbers[index] + numbers[index+1]
+                numbers.pop(index)
+                numbers.pop(index)
+                numbers.insert(index, ans)
+                operator.pop(index)
 
-        elif correct_order[i] == '+':
-            index = operator.index(correct_order[i])
-            ans = numbers[index] + numbers[index+1]
-            numbers.pop(index)
-            numbers.pop(index)
-            numbers.insert(index, ans)
-            operator.pop(index)
+            elif correct_order[i] == '-':
+                index = operator.index(correct_order[i])
+                ans = numbers[index] - numbers[index+1]
+                numbers.pop(index)
+                numbers.pop(index)
+                numbers.insert(index, ans)
+                operator.pop(index)
 
-        elif correct_order[i] == '-':
-            index = operator.index(correct_order[i])
-            ans = numbers[index] - numbers[index+1]
-            numbers.pop(index)
-            numbers.pop(index)
-            numbers.insert(index, ans)
-            operator.pop(index)
+    else:
+        return 'Please check your expression. Unidentified error found.'
         
     return ans
--- numbr.py
+++ numbr.py
@@ -3,18 +3,41 @@ def numbers_sort(exp):
     symbols='//**-+%^!'
     n=''
     i=0
+
     while i < len(exp):
         if exp[i] in digit:
            n+=exp[i]
+
+        elif exp[i] == ' ':
+            i+=1
+            continue
+        
         elif exp[i] in symbols:
-            if exp[i+1] in digit or exp[i+1] == ' ':
+            if exp[i+1] in digit:
                 n+=' '
+
             elif exp[i+1] in symbols:
                 n+=' '
                 i+=1
+
+            elif exp[i+1] == ' ':
+                n+=' '
+                i+=1
+                continue
+
+        else:
+            n=0
+            break
+
+            
         i+=1
-    num_str=n.split(' ')
-    num=[]
-    for i in num_str:
-        num.append(float(i))
-    return num
+
+    if n!=0:
+        num_str=n.split(' ')
+        num=[]
+        for i in num_str:
+            num.append(float(i))
+        return num
+    
+    else:
+        return 0

2e5f716a85c6fdddded34d98189386ac4da1ed58
Aayush-2103 <2103aayush@gmail.com>
2024-05-02 20:09:41 +0530

    ''' ..... ''' this part didn't work well so i made changes in numbr.py

--- clcultion.py
+++ clcultion.py
@@ -19,22 +19,6 @@ def calculation(numbers, correct_order, operator):
                 operator.pop(index)
 
             elif correct_order[i] == '//':
-                '''
-                index = operator.index(correct_order[i])
-                if ceil(numbers[index])==numbers[index] and ceil(numbers[index+1])==numbers[index+1]:
-                    ans = int(numbers[index]) // int(numbers[index+1])
-                    numbers.pop(index)
-                    numbers.pop(index)
-                    numbers.insert(index, ans)
-                    operator.pop(index)
-
-                else:
-                    ans = numbers[index] // numbers[index+1]
-                    numbers.pop(index)
-                    numbers.pop(index)
-                    numbers.insert(index, ans)
-                    operator.pop(index)
-                '''
                 index = operator.index(correct_order[i])
                 ans = numbers[index] // numbers[index+1]
                 numbers.pop(index)
